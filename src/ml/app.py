279cf8d1cd71ad10bd0c202341c32f1faa9f8597
Shuvashree Bharati <shuvashreebharati@example.com>
2026-06-04 14:58:42 +0545

    delete downloads

--- src/ml/app.py
+++ src/ml/app.py
@@ -12,9 +12,6 @@ from dotenv import load_dotenv
 from sklearn.metrics.pairwise import cosine_similarity
 
 import nltk
-nltk.download('stopwords', quiet=True)
-nltk.download('wordnet', quiet=True)
-
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
