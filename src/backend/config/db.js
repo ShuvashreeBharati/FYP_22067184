0687e84b0deee5da410e5711057c367241c87c3d
Shuvashree Bharati <shuvashreebharati@example.com>
2026-06-06 11:34:55 +0545

    db.js edit

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "symptom_diagnosing_tool_db",
-  password: process.env.DB_PASSWORD || "shuvashree",
-  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.connect()
